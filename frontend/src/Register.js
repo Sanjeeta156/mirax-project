1e40f5adea754ac7efaf5d8f5c6f20d9abbd1f18
Sanjeeta156 <sanjeetasingh54@gmail.com>
2026-05-25 12:41:38 +0530

    added signup api

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -10,7 +10,7 @@ function Register() {
     try {
 
       const response = await fetch(
-        "https://mirax-project-production.up.railway.app/signup",
+        "https://mirax-project-production.up.railway.app/register",
        {
         method: "POST",
 
